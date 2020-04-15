09b2ede6470e8b9debdfa7f51c56eae9cf649427
lokmingjay <jay.ngan@oocl.com>
2020-04-15 19:37:34 +0800

    [NGANJA] add changeEmployee

--- src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
@@ -22,18 +22,26 @@ public class EmployeeController {
             new Employee(4, "Wesley", 40, "Male"),
             new Employee(5, "Hilary", 50, "Female")));
 
-
     @GetMapping
     public List<Employee> getAllEmployees() {
         return employees;
     }
 
-
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Employee createNewEmployee(@RequestBody Employee employee){
+    public Employee createNewEmployee(@RequestBody Employee employee) {
         employees.add(employee);
         return employee;
     }
+
+    @PutMapping("/{employeeId}")
+    public Employee changeEmployee(@PathVariable("employeeId")  Integer employeeId,@RequestBody Employee newEmployee) {
+        employees.removeIf(employee ->employee.getId() == employeeId);
+        employees.add(newEmployee);
+        return newEmployee;
+    }
+
+
+
 
 }
