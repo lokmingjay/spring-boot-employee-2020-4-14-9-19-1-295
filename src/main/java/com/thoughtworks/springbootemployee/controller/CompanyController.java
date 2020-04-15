3fe2d7d7779a52516f33157c814c167dd289ed0b
lokmingjay <jay.ngan@oocl.com>
2020-04-15 21:52:58 +0800

    [NGANJA] add company controller

---
+++ src/main/java/com/thoughtworks/springbootemployee/controller/CompanyController.java
@@ -0,0 +1,13 @@
+package com.thoughtworks.springbootemployee.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/company")
+public class CompanyController {
+
+
+
+}
--- src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
@@ -9,9 +9,11 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @RestController
-@RequestMapping("/employee")
+@RequestMapping("/employees")
 public class EmployeeController {
 
 
@@ -22,33 +24,50 @@ public class EmployeeController {
             new Employee(4, "Wesley", 40, "Male"),
             new Employee(5, "Hilary", 50, "Female")));
 
+
     @GetMapping
-    public List<Employee> getAllEmployees() {
+    public List<Employee> getRangeEmployee(@RequestParam(required = false) Integer page,
+                                           @RequestParam(required = false) Integer pageSize,
+                                           @RequestParam(required = false) String gender) {
+        if (gender != null) {
+            return employees.stream().filter(employee -> employee.getGender().equals(gender)).collect(Collectors.toList());
+        }
+
+        if (page != null && pageSize != null) {
+            return employees.subList((page - 1) * pageSize, page * pageSize);
+        }
+
         return employees;
+    }
+
+
+    @GetMapping("/{employeeId}")
+    public Employee getEmployee(@PathVariable("employeeId") Integer employeeId) {
+        return employees.stream().filter(employee -> employee.getId() == employeeId).findFirst().get();
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Employee createNewEmployee(@RequestBody Employee newEmployee) {
-        if(employees.stream().noneMatch(employee -> employee.getId() == newEmployee.getId() )) {
+    public List<Employee> createNewEmployee(@RequestBody Employee newEmployee) {
+        if (employees.stream().noneMatch(employee -> employee.getId() == newEmployee.getId())) {
             employees.add(newEmployee);
-            return newEmployee;
+            return employees;
         }
-            return null;
+        return null;
 
     }
 
     @PutMapping("/{employeeId}")
-    public Employee changeEmployee(@PathVariable("employeeId")  Integer employeeId,@RequestBody Employee newEmployee) {
-        employees.removeIf(employee ->employee.getId() == employeeId);
+    public List<Employee> changeEmployee(@PathVariable("employeeId") Integer employeeId, @RequestBody Employee newEmployee) {
+        employees.removeIf(employee -> employee.getId() == employeeId);
         employees.add(newEmployee);
-        return newEmployee;
+        return employees;
     }
 
     @DeleteMapping("/{employeeId}")
     @ResponseStatus(HttpStatus.ACCEPTED)
-    public List<Employee> removeEmployee(@PathVariable("employeeId")  Integer employeeId){
-        employees.removeIf(employee -> employee.getId()==employeeId);
+    public List<Employee> removeEmployee(@PathVariable("employeeId") Integer employeeId) {
+        employees.removeIf(employee -> employee.getId() == employeeId);
         return employees;
     }
 
